bb27f3b134006bbfaa1ab684c8559008c10e58cf
Roque Chacon Corrales <roke12chacon@gmail.com>
2017-04-05 09:48:49 -0600

    Empezando Proyecto

---
+++ FrmLogin/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FrmLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFacebook = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFacebook
+            // 
+            this.btnFacebook.BackgroundImage = global::FrmLogin.Properties.Resources.fb_logo;
+            this.btnFacebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFacebook.Location = new System.Drawing.Point(229, 141);
+            this.btnFacebook.Name = "btnFacebook";
+            this.btnFacebook.Size = new System.Drawing.Size(151, 137);
+            this.btnFacebook.TabIndex = 0;
+            this.btnFacebook.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".BLACKJACK.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 402);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFacebook);
+            this.Name = "Form1";
+            this.Text = "BlackJack";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFacebook;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
